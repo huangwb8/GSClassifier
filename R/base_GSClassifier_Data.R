b85e98a1a82b8739d078a8aca22dd51d19334921
huangwb8 <huangwb8@654751191@qq.com>
2021-11-30 09:36:43 +0800

    base

--- R/base_GSClassifier_Data.R
+++ R/base_GSClassifier_Data.R
@@ -1,33 +1,43 @@
 
 
-
 #' @title Data in \code{GSClassifier} package
 #' @description Internal dataset of GSClassifier package
-#' @param ImmuneSubtype Data integration for calling of immune subtype by Thorsson et al. in 2018.
-#' @param PAD Data integration for calling of pan-immune activation/dysfunction subtype (PAD).
 #' @param model logic. Whether to only show a list of \code{GSClassifier} models
 #' @author Weibin Huang<\email{654751191@@qq.com}>
+#' @details ImmuneSubtype is a model for personalized calling of immune subtype by Thorsson et al. in 2018. \cr
+#' PAD.train_20200110 is a model for personalized calling of Pan-immune Activation Dysfunction Subtypes(PAD).
 #' @examples
 #' PAD <- readRDS(system.file("extdata", "PAD.train_20200110", package = "GSClassifier"))
 #' ImmuneSubtype <- readRDS(system.file("extdata", "ImmuneSubtype.rds", package = "GSClassifier"))
 #' @export
-GSClassifier_Data <- function(ImmuneSubtype=NULL,
-                              PAD = NULL,
-                              model = T){
-
+GSClassifier_Data <- function(model = T) {
   data.p <- system.file("extdata", package = "GSClassifier")
-  data.n <- list.files(data.p,pattern = '.rds$',full.names = F,recursive = F)
-  if(model) data.n <- data.n[!grepl('test',data.n)]
+  data.n <-
+    list.files(
+      data.p,
+      pattern = '.rds$',
+      full.names = F,
+      recursive = F
+    )
+  if (model)
+    data.n <- data.n[!grepl('test', data.n)]
 
   ## Available data
   message('Available data:')
-  for(i in data.n) cat(' ',i,'\n')
+  for (i in data.n)
+    cat(' ', i, '\n')
 
   ## Example
   message('Usage example: ')
-  cat('  PAD <- readRDS(system.file("extdata", "PAD.train_20200110.rds", package = "GSClassifier"))','\n')
-  cat('  ImmuneSubtype <- readRDS(system.file("extdata", "ImmuneSubtype.rds", package = "GSClassifier"))')
+  cat(
+    '  PAD <- readRDS(system.file("extdata", "PAD.train_20200110.rds", package = "GSClassifier"))',
+    '\n'
+  )
+  cat(
+    '  ImmuneSubtype <- readRDS(system.file("extdata", "ImmuneSubtype.rds", package = "GSClassifier"))'
+  )
 
 }
+
 
 
