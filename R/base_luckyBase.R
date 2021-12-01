bb129fa9510c8ad787d2436cb92223bc6b124c81
huangwb8 <huangwb8@654751191@qq.com>
2021-12-01 09:00:52 +0800

    luckybase

---
+++ R/base_luckyBase.R
@@ -0,0 +1,95 @@
+
+
+
+# usethis::use_package("luckyBase", "Imports"/"Depends"/"Suggests"/"Enhances"/"LinkingTo")
+
+
+####=====================luckyBase functions====================####
+
+#' #' @importFrom luckyBase convert
+#' #' @export
+#' luckyBase::convert
+
+#' #' @importFrom luckyBase convert1
+#' #' @export
+#' luckyBase::convert1
+
+#' #' @importFrom luckyBase convert2
+#' #' @export
+#' luckyBase::convert2
+
+#' #' @importFrom luckyBase Plus.convert
+#' #' @export
+#' luckyBase::Plus.convert
+
+
+#' #' @importFrom luckyBase classify
+#' #' @export
+#' luckyBase::classify
+
+#' #' @importFrom luckyBase confint
+#' #' @export
+#' luckyBase::confint
+
+#' #' @importFrom luckyBase cut_vector
+#' #' @export
+#' luckyBase::cut_vector
+
+
+#' #' @importFrom luckyBase Fastextra
+#' #' @export
+#' luckyBase::Fastextra
+
+#' #' @importFrom luckyBase Fastgrep
+#' #' @export
+#' luckyBase::Fastgrep
+
+
+#' #' @importFrom luckyBase importFromPackage
+#' #' @export
+#' luckyBase::importFromPackage
+
+#' #' @importFrom luckyBase is.one.na
+#' #' @export
+#' luckyBase::is.one.na
+
+#' #' @importFrom luckyBase is.all.na
+#' #' @export
+#' luckyBase::is.all.na
+
+#' #' @importFrom luckyBase is.one.ture
+#' #' @export
+#' luckyBase::is.one.ture
+
+
+#' #' @importFrom luckyBase is.all.ture
+#' #' @export
+#' luckyBase::is.all.ture
+
+
+#' #' @importFrom luckyBase LuckyVerbose
+#' #' @export
+#' luckyBase::LuckyVerbose
+
+#' #' @importFrom luckyBase Plus.library
+#' #' @export
+#' luckyBase::Plus.library
+
+
+#' #' @importFrom luckyBase show.color
+#' #' @export
+#' luckyBase::show.color
+
+
+#' #' @importFrom luckyBase verbose_vector
+#' #' @export
+#' luckyBase::verbose_vector
+
+
+#' #' @importFrom luckyBase mymusic
+#' #' @export
+#' luckyBase::mymusic
+
+
+
+
